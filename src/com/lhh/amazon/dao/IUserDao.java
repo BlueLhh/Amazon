8e6fee58c8f9ef55413dd5ad462859ef5684fbdd
BlueLhh <lanhonghui100@gmail.com>
2017-10-31 14:08:23 +0800

    完成IUserDao的insert（插入数据）方法以及query（查询数据）方法

--- src/com/lhh/amazon/dao/IUserDao.java
+++ src/com/lhh/amazon/dao/IUserDao.java
@@ -1,5 +1,30 @@
 package com.lhh.amazon.dao;
 
-public interface IUserDao {
+import java.sql.Connection;
 
+import com.lhh.amazon.common.DataAccessException;
+import com.lhh.amazon.entity.User;
+
+public interface IUserDao {
+	/**
+	 * 插入数据
+	 * 
+	 * @param conn
+	 * @param user
+	 * @throws DataAccessException
+	 */
+	public void insert(Connection conn, User user) throws DataAccessException;
+
+	/**
+	 * 
+	 * 查询数据
+	 * 
+	 * @param conn
+	 * @param username
+	 * @param password
+	 * @throws DataAccessException
+	 */
+	public void query(Connection conn, String username, String password) throws DataAccessException;
+
+	public void query(Connection conn, String username) throws DataAccessException;
 }
