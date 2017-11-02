66ef27e339e4b626adec482e2fcba0a338907b7f
BlueLhh <lanhonghui100@gmail.com>
2017-11-02 22:23:51 +0800

    完成商品分类的过滤器

---
+++ src/com/lhh/amazon/web/filter/CategoryFilter.java
@@ -0,0 +1,58 @@
+package com.lhh.amazon.web.filter;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+import com.lhh.amazon.common.ServiceException;
+import com.lhh.amazon.entity.ProductCategory;
+import com.lhh.amazon.service.IProductCategoryService;
+import com.lhh.amazon.service.impl.ProductCategoryServiceImpl;
+
+//@WebFilter("/index_product_sort.jsp")
+@WebFilter(filterName = "NewsFilter", urlPatterns = { "/index.jsp", "/guestbook.jsp", "/product_view.jsp" })
+public class CategoryFilter implements Filter {
+
+	public CategoryFilter() {
+
+	}
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		// 查询
+		IProductCategoryService ipcs = new ProductCategoryServiceImpl();
+		List<ProductCategory> list = null;
+
+		try {
+			list = ipcs.findCategory();
+		} catch (ServiceException e) {
+			e.printStackTrace();
+		}
+
+		if (list.size() > -1) {
+			// 查询成功
+			// 保存
+			request.setAttribute("Category", list);
+			// 放行资源
+			chain.doFilter(request, response);
+		} else {
+			// 查询失败
+			System.out.println("没有数据！");
+		}
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
