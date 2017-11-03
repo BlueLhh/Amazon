8c10d509f87b0b6c9c9cbbea3d5ff979d29d80df
BlueLhh <lanhonghui100@gmail.com>
2017-11-03 14:01:47 +0800

    商品的过滤器

---
+++ src/com/lhh/amazon/web/filter/ProductPviewFilter.java
@@ -0,0 +1,49 @@
+package com.lhh.amazon.web.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+import com.lhh.amazon.common.ServiceException;
+import com.lhh.amazon.entity.Product;
+import com.lhh.amazon.service.IProductService;
+import com.lhh.amazon.service.impl.ProductServicempl;
+
+//@WebFilter(filterName = "ProductPviewFilter", urlPatterns = { "/product_view.jsp", "/pview" })
+public class ProductPviewFilter implements Filter {
+	public ProductPviewFilter() {
+	}
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		String id = request.getParameter("id");
+		Long productID = Long.parseLong(id);
+		System.out.println("ID为："+id);
+		IProductService ips = new ProductServicempl();
+		Product product = new Product();
+
+		try {
+			product = ips.findProduct(productID);
+			System.out.println(product);
+			// 查找成功，保存
+			request.setAttribute("product", product);
+		} catch (ServiceException e) {
+			e.printStackTrace();
+		}
+		// 放行进入下一资源
+		chain.doFilter(request, response);
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
