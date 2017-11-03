5a083419f27b648089b07e6bbcb32a9a1df73f81
BlueLhh <lanhonghui100@gmail.com>
2017-11-03 14:01:22 +0800

    完成ProguctPviewServlet

---
+++ src/com/lhh/amazon/web/servlet/ProductPviewServlet.java
@@ -0,0 +1,54 @@
+package com.lhh.amazon.web.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.lhh.amazon.common.ServiceException;
+import com.lhh.amazon.entity.Product;
+import com.lhh.amazon.service.IProductService;
+import com.lhh.amazon.service.impl.ProductServicempl;
+
+@WebServlet("/pview")
+public class ProductPviewServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public ProductPviewServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// response.getWriter().append("Served at:
+		// ").append(request.getContextPath());
+		// 获取点击之后的ID
+		String id = request.getParameter("id");
+		Long productID = Long.parseLong(id);
+		
+		IProductService ips = new ProductServicempl();
+		Product product = new Product();
+		
+		try {
+			product = ips.findProduct(productID);
+			//查找成功，保存
+			request.setAttribute("product", product);
+			//跳转界面
+			//response.sendRedirect("product_view.jsp");
+			RequestDispatcher rd = request.getRequestDispatcher("product_view.jsp");
+			rd.forward(request, response);
+		} catch (ServiceException e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
